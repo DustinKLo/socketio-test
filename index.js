2f887ef46e69477b78d120b27358c3c218196cad
Dustin Lo <dustinlo@users-MacBook-Pro-2.local>
2019-09-13 22:19:31 -0700

    who is online

--- index.js
+++ index.js
@@ -3,35 +3,46 @@ var http = require('http').createServer(app);
 var io = require('socket.io')(http);
 
 let totalOnlineUsers = 0;
+let usersOnline = [];
 
 app.get('/', function(req, res) {
 	res.sendFile(__dirname + '/index.html');
 });
 
-io.on('connection', (socket) => {
+io.on('connection', socket => {
+  console.log('user connected');
   totalOnlineUsers++;
-  console.log('a user connected');
+  
+  socket.on('chat message', payload => {
+		io.emit('chat message', payload);
+	});
+
+	io.emit('users online', { totalUsers: totalOnlineUsers });
+
+	socket.on('user typing', payload => {
+		io.emit('user typing', payload);
+  });
+
+  socket.on('user joined', payload => {
+    usersOnline.push(payload);
+    io.emit('user joined', usersOnline);
+  });
+
+  io.emit('user joined', usersOnline);
 
   socket.on('disconnect', () => {
+    console.log('user disconnected');
     totalOnlineUsers--;
-    console.log('a user disconnected');
     io.emit('users online', { usersOnline: totalOnlineUsers });
-  });
 
-  socket.on('chat message', (payload) => {
-    console.log(payload);
-    io.emit('chat message', payload);
-  });
-
-  io.emit('users online', { usersOnline: totalOnlineUsers });
-
-  socket.on('user typing', (payload) => {
-    io.emit('user typing', payload);
-  });
-
-  console.log(`${totalOnlineUsers} users online`);
+    var i = usersOnline.findIndex(x => x.socketId === socket.id);
+    if (i > -1) {
+      usersOnline.splice(i, 1);
+    }
+    io.emit('user left', usersOnline);
+    io.emit('whos online', usersOnline);
+	});
 });
-
 
 const PORT = process.env.PORT || 5000;
 http.listen(PORT, function() {
